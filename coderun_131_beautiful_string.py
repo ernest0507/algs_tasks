6e3a7bfb6bb5672ae544f3e971b991e78886ecb8
ergoog <ergoog51@gmail.com>
2025-04-30 10:11:24 +0500

    solution

--- coderun_131_beautiful_string.py
+++ coderun_131_beautiful_string.py
@@ -1,15 +1,18 @@
-s = input()
 k = int(input())
+s = input()
 
-for i in range(len(s) - 1):
-    j = len(s) - 1
-    while i < j:
-        if s[i] == s[j] and j - i - 1== k:
-            print(j - i + 1)
-            quit()
+i, j = 0, 1
+mcount = 0
+while j < len(s):
+    if s[i] == s[j]:
+        if k + i + 1 < len(s):
+            mcount = max(mcount, k + 2)
         else:
-            j -= 1
+            mcount = max(mcount, len(s) - i)
+        j += 1
+    elif j - i - 1 != k:
+        j += 1
+    else:
+        i += 1
 
-print(k + 1)
-
-
+print(k + 1) if mcount == 0 else print(mcount)
