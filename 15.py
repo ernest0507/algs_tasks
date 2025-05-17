a965692b5b46a081d6ea2f9a0c5a339f145c29fe
ergoog <ergoog51@gmail.com>
2025-05-17 20:12:06 +0500

    solution

--- 15.py
+++ 15.py
@@ -1,22 +1,36 @@
-n = int(input()) # кол-во коробок
-boxes = [[int(x) for x in input().split()] for _ in range(n)] + [[0, 0]]
+n = int(input())
+boxes = [[int(x) for x in input().split()] for _ in range(n)] + [[]]
 
-boxes[-1] = [boxes[0][1], 0]
-boxes[0][1] = 0
-for i in range(n + 1):
-    if boxes[i][1] == 0:
-        find = boxes[i][0]
-        for j in range(i + 1, n + 1):
-            if boxes[j][1] == find:
-                boxes[i][1] = find
-                boxes[j][1] = 0
-                break
-            if boxes[j][1] == 0 and boxes[j][0] == find:
-                boxes[i][1] = find
-                boxes[j][0] = 0
-                break
-
-
+dct_ind = {}  # словарь для хранения текущего расположения шара
+for i in range(n):
+    dct_ind[boxes[i][0]] = dct_ind.get(boxes[i][0], []) + [i]
+    dct_ind[boxes[i][1]] = dct_ind.get(boxes[i][1], []) + [i]
+count = 0
+for i in range(n):
+    balloon1, balloon2 = boxes[i]
+    if balloon1 != balloon2:
+        boxes[n].append(balloon2)
+        ind1 = dct_ind[balloon1][1]
+        if boxes[ind1][1] == balloon1:
+            boxes[i] = [balloon1, balloon1]
+            boxes[ind1][1] = boxes[n][0]
+            dct_ind[balloon1] = [i, i]
+            boxes[n] = []
+            if dct_ind[balloon2][1] == ind1:
+                dct_ind[balloon2][1] = ind1
+            else:
+                dct_ind[balloon2][0] = ind1
+            count += 3
+        else:
+            boxes[n].append(boxes[ind1][1])
+            boxes[i] = [balloon1, balloon1]
+            boxes[ind1] = boxes[n]
+            boxes[n] = []
+            if dct_ind[balloon2][0] == i:
+                dct_ind[balloon2][0] = ind1
+            else:
+                dct_ind[balloon2][1] = ind1
+            count += 4
 
 print(boxes)
-
+print(count)
--- main.py
+++ main.py
@@ -1,22 +1,27 @@
-n = int(input()) # кол-во коробок
-boxes = [[int(x) for x in input().split()] for _ in range(n)] + [[0, 0]]
+n = int(input())
+boxes = [[int(x) for x in input().split()] for _ in range(n)] + [[]]
 
-boxes[-1] = [boxes[0][1], 0]
-boxes[0][1] = 0
-for i in range(n + 1):
-    if boxes[i][1] == 0:
-        find = boxes[i][0]
-        for j in range(i + 1, n + 1):
-            if boxes[j][1] == find:
-                boxes[i][1] = find
-                boxes[j][1] = 0
-                break
-            if boxes[j][1] == 0 and boxes[j][0] == find:
-                boxes[i][1] = find
-                boxes[j][0] = 0
-                break
+dct_ind = {}  # словарь для хранения текущего расположения шара
+for i in range(n):
+    dct_ind[boxes[i][0]] = dct_ind.get(boxes[i][0], []) + [i]
+    dct_ind[boxes[i][1]] = dct_ind.get(boxes[i][1], []) + [i]
+dct_ind[n] = []
 
-
-
-print(boxes)
-
+for i in range(n):
+    balloon1, balloon2 = boxes[i]
+    if balloon1 != balloon2:
+        boxes[n].append(balloon2)
+        # случай, когда нужный шар сверху
+        if dct_ind[balloon1][1] != i:
+            if dct_ind[balloon2][0] == dct_ind[balloon1][0]: # проверяем какую позицию в boxes нужно переписывать
+                boxes[dct_ind[balloon1][1]][1] = balloon2 # переписываем элемент в boxes
+                dct_ind[balloon2][0] = dct_ind[balloon1][1] # переписываем индекс для balloon2
+            else:
+                boxes[dct_ind[balloon1][0]][1] = balloon2
+                dct_ind[balloon2][1] = dct_ind[balloon1][1]
+            boxes[i] = [balloon1, balloon1]
+        else:
+            ind = boxes[dct_ind[balloon1][1]][1]
+            boxes[n].append()
+            boxes[i] = [balloon1, balloon1]
+            boxes[]
