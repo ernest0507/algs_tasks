de1575976fa53835a9eca8252d3fd7861b58046a
ergoog <ergoog51@gmail.com>
2025-05-18 07:02:20 +0500

    solution

--- 15.py
+++ 15.py
@@ -5,33 +5,66 @@ dct_ind = {}  # словарь для хранения текущего расп
 for i in range(n):
     dct_ind[boxes[i][0]] = dct_ind.get(boxes[i][0], []) + [i]
     dct_ind[boxes[i][1]] = dct_ind.get(boxes[i][1], []) + [i]
+
 count = 0
+sequences = []
 for i in range(n):
     balloon1, balloon2 = boxes[i]
     if balloon1 != balloon2:
-        boxes[n].append(balloon2)
-        ind1 = dct_ind[balloon1][1]
-        if boxes[ind1][1] == balloon1:
-            boxes[i] = [balloon1, balloon1]
-            boxes[ind1][1] = boxes[n][0]
-            dct_ind[balloon1] = [i, i]
-            boxes[n] = []
-            if dct_ind[balloon2][1] == ind1:
-                dct_ind[balloon2][1] = ind1
-            else:
-                dct_ind[balloon2][0] = ind1
-            count += 3
+        if dct_ind[balloon1][0] != i:  # получаю индекс шара, которой нужно переложить
+            ind = dct_ind[balloon1][0]
         else:
-            boxes[n].append(boxes[ind1][1])
-            boxes[i] = [balloon1, balloon1]
-            boxes[ind1] = boxes[n]
-            boxes[n] = []
-            if dct_ind[balloon2][0] == i:
-                dct_ind[balloon2][0] = ind1
-            else:
-                dct_ind[balloon2][1] = ind1
-            count += 4
+            ind = dct_ind[balloon1][1]
 
-print(boxes)
+        # перекладываю второй шар в пустую коробку
+        boxes[n].append(balloon2)
+        sequences.append([i, n])
+        count += 1
+
+        # проверяем снизу или сверху находится необходимый шар
+        # если сверху
+        if boxes[ind][1] == balloon1:
+            # просто перекидываем шар к своему цвету
+            boxes[i] = [balloon1, balloon1]
+            sequences.append([ind, i])
+            count += 1
+            boxes[n] = []
+            # перекладываем второй шар на новое место
+            boxes[ind][1] = balloon2
+            sequences.append([n, ind])
+            count += 1
+
+            # переписываем переменные
+            dct_ind[balloon1] = [i, i]
+            if dct_ind[balloon2][0] == i:
+                dct_ind[balloon2][0] = ind
+            else:
+                dct_ind[balloon2][1] = ind
+        # если снизу
+        else:
+            boxes[n].append(boxes[ind][1])
+            count += 1
+
+            # соединяю два шара одного цвета вместе
+            boxes[i] = [balloon1, balloon1]
+            sequences.append([ind, i])
+            count += 1
+
+            # перекладываю два шара из запасной коробки в новую
+            boxes[ind] = boxes[n]
+            sequences.append([n, ind])
+            sequences.append([n, ind])
+            boxes[n] = []
+            count += 2
+
+            # переписываю индексы для шаров
+            dct_ind[balloon1] = [i, i]
+
+            if dct_ind[balloon2][0] == i:
+                dct_ind[balloon2][0] = ind
+            else:
+                dct_ind[balloon2][1] = ind
 
 print(count)
+for s in sequences:
+    print(*s)
--- main.py
+++ main.py
@@ -5,23 +5,67 @@ dct_ind = {}  # словарь для хранения текущего расп
 for i in range(n):
     dct_ind[boxes[i][0]] = dct_ind.get(boxes[i][0], []) + [i]
     dct_ind[boxes[i][1]] = dct_ind.get(boxes[i][1], []) + [i]
-dct_ind[n] = []
 
+count = 0
+sequences = []
 for i in range(n):
     balloon1, balloon2 = boxes[i]
     if balloon1 != balloon2:
-        boxes[n].append(balloon2)
-        # случай, когда нужный шар сверху
-        if dct_ind[balloon1][1] != i:
-            if dct_ind[balloon2][0] == dct_ind[balloon1][0]: # проверяем какую позицию в boxes нужно переписывать
-                boxes[dct_ind[balloon1][1]][1] = balloon2 # переписываем элемент в boxes
-                dct_ind[balloon2][0] = dct_ind[balloon1][1] # переписываем индекс для balloon2
-            else:
-                boxes[dct_ind[balloon1][0]][1] = balloon2
-                dct_ind[balloon2][1] = dct_ind[balloon1][1]
-            boxes[i] = [balloon1, balloon1]
+        if dct_ind[balloon1][0] != i:  # получаю индекс шара, которой нужно переложить
+            ind = dct_ind[balloon1][0]
         else:
-            ind = boxes[dct_ind[balloon1][1]][1]
-            boxes[n].append()
+            ind = dct_ind[balloon1][1]
+
+        # перекладываю второй шар в пустую коробку
+        boxes[n].append(balloon2)
+        sequences.append([i, n])
+        count += 1
+
+        # проверяем снизу или сверху находится необходимый шар
+        # если сверху
+        if boxes[ind][1] == balloon1:
+            # просто перекидываем шар к своему цвету
             boxes[i] = [balloon1, balloon1]
-            boxes[]
+            sequences.append([ind, i])
+            count += 1
+            boxes[n] = []
+            # перекладываем второй шар на новое место
+            boxes[ind][1] = balloon2
+            sequences.append([n, ind])
+            count += 1
+
+            # переписываем переменные
+            dct_ind[balloon1] = [i, i]
+            if dct_ind[balloon2][0] == i:
+                dct_ind[balloon2][0] = ind
+            else:
+                dct_ind[balloon2][1] = ind
+        # если снизу
+        else:
+            boxes[n].append(boxes[ind][1])
+            count += 1
+
+            # соединяю два шара одного цвета вместе
+            boxes[i] = [balloon1, balloon1]
+            sequences.append([ind, i])
+            count += 1
+
+            # перекладываю два шара из запасной коробки в новую
+            boxes[ind] = boxes[n]
+            sequences.append([n, ind])
+            sequences.append([n, ind])
+            boxes[n] = []
+            count += 2
+
+            # переписываю индексы для шаров
+            dct_ind[balloon1] = [i, i]
+
+            if dct_ind[balloon2][0] == i:
+                dct_ind[balloon2][0] = ind
+            else:
+                dct_ind[balloon2][1] = ind
+
+
+print(count)
+for s in sequences:
+    print(*s)
